79ca27759bfc248cdeb40938c6b8453242cee3ae
syohannes50 <syohannes50@gmail.com>
2024-08-08 07:29:15 -0400

    Update and rename delete.py to workingledcode.py

--- workingledcode.py
+++ workingledcode.py
@@ -24,30 +24,6 @@ global Buzz
 
 Buzz = GPIO.PWM(Buzzer, 1000)
 
-# def setup(Rpin):
-# 	global pins
-# 	global p_R
-# 	pins = {'pin_R': Rpin}     
-# 	for i in pins:
-# 		GPIO.setup(pins[i], GPIO.OUT)   # Set pins' mode is output
-# 		GPIO.output(pins[i], GPIO.HIGH) # Set pins to high(+3.3V) to off led
-# 	p_R = GPIO.PWM(pins['pin_R'], 2000)  # set Frequece to 2KHz
-# 
-# def map(x, in_min, in_max, out_min, out_max):
-# 	return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
-
-# def off():
-# 	for i in pins:
-# 		GPIO.setup(pins[i], GPIO.OUT)   # Set pins' mode is output
-# 		GPIO.output(pins[i], GPIO.HIGH)    # Turn off all leds
-# # 
-# def setColor(col):   # For example : col = 0x112233
-# 	R_val = (col & 0xff0000) >> 16
-# 	R_val = map(R_val, 0, 255, 0, 100)
-# 	p_R.ChangeDutyCycle(100-R_val)     # Change duty cycle
-
-
-
 def loop(cond):
     if (cond == 1):
         GPIO.output(R, GPIO.HIGH)
